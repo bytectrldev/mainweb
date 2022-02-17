c3b8c6170cf162eaa7291fdbc2f234b5e9d669ab
Mayur Gajula <50752899+mayurgajula@users.noreply.github.com>
2022-02-17 17:00:55 +0530

    Footer Code
    Footer UI completed

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import '../../Components/Footer/Footer.css'
+import '../../Components/Footer/Footer.css';
+import {Container, Row, Col} from 'react-bootstrap';
+ import logo from '../../Components/Footer/FooterAssets/bytectrltext.png';
+import { FaDiscord,  FaFacebookF, FaInstagram, FaWhatsapp, } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+// import Gradient from 'rgt';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Footer = () => {
-  return 
-  <div>
-    <Containter fluid className='footercontainer'>
-
-    </Containter>
-  </div>;
+  return(
+      <div>
+        <Container fluid>
+          <Row className='p-2 bg-dark' xs={2} md={4} lg={6}>
+            <Col xs={12} md={4} className="pt-3">
+              <img src={logo} alt='logo' height='50em'/>
+            </Col>
+            <Col xs={12} md={4} className="text-center">
+            <IconContext.Provider value={{ color: "white", size:"2.8em", className: "global-class-name footer_socialicons" }}>
+              <a href='#'><FaFacebookF/></a>
+              <a href='https://www.instagram.com/bytectrl/'><FaInstagram/></a>
+              <a href='https://discord.gg/9jZ9jg4t'><FaDiscord/></a>
+              <a href='#'><FaWhatsapp/></a>
+            </IconContext.Provider>
+            </Col>
+            <Col xs={12} md={4} style={{fontSize:"1.5em"}}  className="text-muted text-center">
+                Copyright © 2022 All rights reserved by <span style={{color:'whitesmoke'}}>ByteCtrl</span>.
+            </Col>
+          </Row> 
+        </Container>
+      </div>
+  );
 };
 
 export default Footer;
