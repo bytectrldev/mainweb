5933281b8c3cb82c962b9f0b63ba004c52117b21
Mayur Gajula <50752899+mayurgajula@users.noreply.github.com>
2022-02-17 12:14:39 +0530

    container
    added one container

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../../Components/Footer/Footer.css'
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return 
+  <div>
+    <Containter fluid className='footercontainer'>
+
+    </Containter>
+  </div>;
 };
 
 export default Footer;
