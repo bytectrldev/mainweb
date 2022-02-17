1eaff4e6fa520041e54524f1e34ecafdcce734b2
sghere <gshubham8@gmail.com>
2022-02-18 00:03:25 +0530

    18/02 Final Commit

--- src/Components/Footer/FooterComponents/SocialMedia/SocialIcons.js
+++ src/Components/Footer/FooterComponents/SocialMedia/SocialIcons.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from 'react';
 import socialmediadata from './socialmediadata';
-import { IconContext } from "react-icons";
-import '../SocialMedia/SocialMediaIcons.css'
+import { IconContext } from 'react-icons';
+import '../SocialMedia/SocialMediaIcons.css';
 
 function SocialIcons() {
   return (
     <>
-        {socialmediadata.map((data)=>
-            {
-                return(
-                    <>
-                        <IconContext.Provider value={{ color: "white", size:"2.8em", className: "global-class-name socialicons" }}>
-                            <a href={data.href}>{data.elements}</a>
-                        </IconContext.Provider>
-                    </>
-                );
-            }
-        )}
+      {socialmediadata.map((data) => {
+        return (
+          <>
+            <IconContext.Provider
+              value={{
+                color: 'white',
+                size: '2.8em',
+                className: 'global-class-name socialicons',
+              }}
+            >
+              <a href={data.href}>{data.elements}</a>
+            </IconContext.Provider>
+          </>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default SocialIcons
+export default SocialIcons;
