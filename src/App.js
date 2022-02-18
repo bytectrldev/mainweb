df3e3d0dbe5933d367cbca55d1ec70bc5921e796
sghere <gshubham8@gmail.com>
2022-02-19 01:09:25 +0530

    Little Designing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './Pages/Landing';
+import { useState } from 'react';
 
 // import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const [Theme, setTheme] = useState(true);
   return (
     // <BrowserRouter>
     <div className="App">
-      <Landing />
+      <Landing Theme={Theme} setTheme={setTheme} class="ALL" />
     </div>
     // </BrowserRouter>
   );
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../../Components/Footer/Footer.css';
-import {Container, Row, Col} from 'react-bootstrap';
-import logo from '../../Components/Footer/FooterAssets/bytectrltext.png';
+import { Container, Row, Col } from 'react-bootstrap';
+// import logo from '../../Components/Footer/FooterAssets/bytectrltext.png';
 import SocialIcons from './FooterComponents/SocialMedia/SocialIcons';
 // import Gradient from 'rgt';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const Footer = (props) => {
-  return(
-      <div className={props.Theme ?'light_theme':'dark_theme'} style={{minWidth:"320px"}}>
-        <Container>
-          <Row className='p-2' xs={2} md={4} lg={6}>
-            <Col xs={12} md={6} className="text-center">
-                <SocialIcons/>
-            </Col>
-            <Col xs={12} md={6} className="text-muted copyright_text text-center">
-              Copyright © 2022 All rights reserved by <span>ByteCtrl</span>.
-            </Col>
-          </Row> 
-        </Container>
-      </div>
+  return (
+    <div
+      className={props.Theme ? 'light_theme' : 'dark_theme'}
+      style={{ minWidth: '320px' }}
+    >
+      <Container>
+        <Row className="p-2" xs={2} md={4} lg={6}>
+          <Col xs={12} md={6} className="text-center">
+            <SocialIcons />
+          </Col>
+          <Col xs={12} md={6} className="text-muted copyright_text text-center">
+            Copyright © 2022 All rights reserved by <span>ByteCtrl</span>.
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
