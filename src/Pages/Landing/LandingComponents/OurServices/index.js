14c55ff913432af0415a62a2435ba0bb1715b5ed
sghere <gshubham8@gmail.com>
2022-02-22 01:05:38 +0530

    minor

--- src/Pages/Landing/LandingComponents/OurServices/index.js
+++ src/Pages/Landing/LandingComponents/OurServices/index.js
@@ -1,32 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import ComponentHeading from '../ComponentHeading';
 import './OurServices.css';
 import ServicesCard from './OurServicesComponents/ServicesCard';
 
 import { CgWebsite } from 'react-icons/cg';
-import { FaGooglePlay, FaBezierCurve } from 'react-icons/fa';
-
-// import data from 'https://mahalaxmisanitary.com/test.json';
+// import { FaGooglePlay, FaBezierCurve } from 'react-icons/fa';
 
 const OurServices = (props) => {
-  // fetch('https://mahalaxmisanitary.com/test.json', {
-  //   mode: 'no-cors',
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // }).then((res) => {
-  //   console.log(res);
-  // });
   var classname = 'OurServices dark';
   var classname1 = 'OurServices light';
+
+  var DataMayur = {
+    OurServices: {
+      Card: [
+        {
+          id: 1,
+          icon: 'CgWebsite',
+          Heading: 'Website Development',
+          description: 'A strategic approach to website design and development',
+        },
+        {
+          id: 2,
+          icon: 'FaGooglePlay',
+          Heading: 'App Development',
+          description: 'Life is too short for ordinary apps.',
+        },
+        {
+          id: 3,
+          icon: 'FaBezierCurve',
+          Heading: 'Business Development',
+          description: 'You’ve got us? You’ve got class.',
+        },
+      ],
+    },
+    TeamMembers: {
+      Team: [
+        {
+          id: 1,
+          photo: 'https://source.unsplash.com/random',
+          name: 'SHUBHAM GAIKWAD',
+          profession: 'DEVELOPER',
+          instagram: 'https://www.instagram.com/bytectrl/',
+          btnlink: 'shubham',
+        },
+        {
+          id: 2,
+          photo: 'https://source.unsplash.com/random',
+          name: 'NISHANT JOSHI',
+          profession: 'DEVELOPER',
+          instagram: 'https://www.instagram.com/bytectrl/',
+          btnlink: 'nishant',
+        },
+        {
+          id: 3,
+          photo: 'https://source.unsplash.com/random',
+          name: 'MAYUR GAJULA',
+          profession: 'DEVELOPER',
+          instagram: 'https://www.instagram.com/bytectrl/',
+          btnlink: 'mayur',
+        },
+      ],
+    },
+  };
+
+  const [Cards, setCards] = useState();
+
+  // const DATA = fetch('https://mahalaxmisanitary.com/test.json')
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     alert(err);
+  //   });
+
+  fetch('https://mahalaxmisanitary.com/test.json')
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result.OurServices[0]);
+    });
+
   return (
     <Container fluid className="text-center OurServicesBG">
       <Container className={props.Theme ? classname1 : classname}>
-        <ComponentHeading Heading="What we can do for you" Heading2="Our Awesome Service" />
+        <ComponentHeading
+          Heading="What we can do for you"
+          Heading2="Our Awesome Service"
+        />
         <div className="row">
-          <ServicesCard
+          {/* <ServicesCard
             icon={<CgWebsite />}
             Heading="Website Development"
             description="A strategic approach to website design and development"
@@ -40,7 +102,7 @@ const OurServices = (props) => {
             icon={<FaBezierCurve />}
             Heading="Business Development"
             description="You’ve got us? You’ve got class."
-          />
+          /> */}
         </div>
       </Container>
     </Container>
